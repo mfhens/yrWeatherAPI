560372685217493c3440e0a56aea40becbdeaa5c
Markus Friede Hens <markus@friedehens.eu>
2018-06-19 12:04:54 +0200

    Changed approach to use HTML directly

--- yrWeatherAPI.py
+++ yrWeatherAPI.py
@@ -1,35 +1,71 @@
 import urllib.request
+import configparser
+import yrconfig
 from lxml import etree
 
-urlPattern = "http://www.yr.no/place/{}/forecast.xml"
-location = "Denmark/Zealand/Asnæs"
-xsltFilePath = "transformToHTML.xslt"
+url_prefix = yrconfig.url_prefix
+url_pattern = yrconfig.url_pattern
+yr_xml = yrconfig.xml
+location = yrconfig.location
 
-def generateUrl(fullLocation):
+def generateUrl(urlPattern, fullLocation):
     fullLocation = properEncode(fullLocation)
     return urlPattern.format(fullLocation)
 
 def properEncode(url):
-  url = url.replace("ø", "%C3%B8")
-  url = url.replace("å", "%C3%A5")
-  url = url.replace("æ", "%C3%A6")
-  url = url.replace("Ø", "%C3%98")
-  url = url.replace("Å", "%C3%A5")
-  url = url.replace("Æ", "%C3%85")
+    url = url.replace("ø", "%C3%B8")
+    url = url.replace("å", "%C3%A5")
+    url = url.replace("æ", "%C3%A6")
+    url = url.replace("Ø", "%C3%98")
+    url = url.replace("Å", "%C3%A5")
+    url = url.replace("Æ", "%C3%85")
   return url
 
-def retrieveForecast(fullLocation):
-    url = generateUrl(fullLocation)
+def retrieveForecastXml(fullLocation):
+    url = generateUrl(url_pattern, fullLocation) + yr_xml
     response = urllib.request.urlopen(url)
     forecast = response.read()
     return forecast
 
+def retrieveForecastHtml(fullLocation):
+    url = generateUrl(url_pattern, fullLocation)
+    response = urllib.request.urlopen(url)
+    forecast = response.read()
+    return forecast
+
+def remove_unnecessary(htmltree):
+    for node in htmltree.xpath("//div[starts-with(@class, 'yr-webcams')]"):
+        node.getparent().remove(node)
+    for node in htmltree.xpath("//header"):
+        node.getparent().remove(node)
+    for node in htmltree.xpath("//div[starts-with(@class, 'yr-footer')]"):
+        node.getparent().remove(node)
+    for node in htmltree.xpath("//div[starts-with(@class, 'yr-breadcrumbs')]"):
+        node.getparent().remove(node)
+    for node in htmltree.xpath("//li[@class='related']"):
+        node.getparent().remove(node)
+    for node in htmltree.xpath("//div[@id='yr-timestamp-tools']"):
+        node.getparent().remove(node)
+    for node in htmltree.xpath("//div[starts-with(@class, 'yr-pointinfo')]"):
+        node.getparent().remove(node)
+    for node in htmltree.xpath("//div[starts-with(@class, 'yr-map')]"):
+        node.getparent().remove(node)
+    return htmltree
+
+def prefix_links(htmltree):
+    for node in htmltree.xpath("//link[@rel='stylesheet']"):
+        node.attrib['href'] = url_prefix + node.attrib['href']
+    for node in htmltree.xpath("//img"):
+        node.attrib['src'] = url_prefix + node.attrib['src']
+    return htmltree
+
 try:
-    forecast = retrieveForecast(location)
-    root = etree.fromstring(forecast)
-    xslt_doc = etree.parse(xsltFilePath)
-    transform = etree.XSLT(xslt_doc)
-    result = transform(root)
-    print(result)
+    forecast = retrieveForecastHtml(location)
+    parser = etree.HTMLParser()
+    htmltree = etree.fromstring(forecast, parser)
+    htmltree = remove_unnecessary(htmltree)
+    htmltree = prefix_links(htmltree)
+    htmlPage = open("test.html", "wb")
+    htmlPage.write(etree.tostring(htmltree, pretty_print=True, method="HTML"))
 except RuntimeError as error:
     print("Can't read forecast: {0}".format(error))
---
+++ yrconfig.py
@@ -0,0 +1,11 @@
+
+url_prefix = "http://www.yr.no"
+url_pattern = "https://www.yr.no/place/{}/"
+xml = "forecast.xml"
+hour_by_hour = "hour_by_hour.html"
+longterm = "long.html"
+
+location = "Denmark/Zealand/Asnæs"
+
+output_path = "."
+output_html = "index.html"
